f07c014e14881c25cebb5fd2eb8ebae149dd7e28
Johann Cardona <2007048@brunel.ac.uk>
2024-11-20 18:03:41 +0000

    Create endpoint for generating artistic images with DALL-E

---
+++ backend/art_generation/DALLE3_img.py
@@ -0,0 +1,34 @@
+import openai
+from flask import Blueprint, request, jsonify
+from http import HTTPStatus
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+generation = Blueprint("generation", __name__)
+
+client = openai.OpenAI(
+    organization=os.environ.get('organization'),
+    project=os.environ.get('project'),
+    api_key=os.environ.get('api_key')
+)
+
+
+@generation.route("/image_generation", methods=["POST"])
+def get_user_prompt():
+
+    data = request.get_json()
+    print(data["prompt"])
+    response = client.images.generate(
+        model="dall-e-3",
+        prompt=data["prompt"],
+        size="1024x1024",
+        quality="standard",
+        n=1
+    )
+
+    img_url = response.data[0].url
+    print(img_url)
+
+    return jsonify({"result": img_url}, HTTPStatus.OK)
