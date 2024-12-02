9ca7576331deb1198483d5a3ed1e9e720b69e1ae
Johann Cardona <2007048@brunel.ac.uk>
2024-12-02 09:56:37 +0000

    Updates to emotion classifier
    - Update to model structure
    - Having conditional paths for training and predictions
    - Saving best model
    - Evaluating model with accuracy and loss

--- backend/textEmotion/emotionClassifier.py
+++ backend/textEmotion/emotionClassifier.py
@@ -18,7 +18,7 @@ def train_val_test_split(df: pd.DataFrame):
     print(df["text"].head().tolist())
     print(df["label"].head().tolist())
     X_train, X_temp, y_train, y_temp = model_selection.train_test_split(
-        df["text"].to_list(), df["label"].to_list(), train_size=0.6, random_state=42, shuffle=True)
+        df["text"].to_list(), df["label"].to_list(), train_size=0.7, random_state=42, shuffle=True)
     X_val, X_test, y_val, y_test = model_selection.train_test_split(
         X_temp, y_temp, test_size=0.5, random_state=42, shuffle=True)
     return X_train, X_val, X_test, y_train, y_val, y_test
@@ -35,14 +35,12 @@ def tokenize_sentences(X_train_text, X_val_text, X_test_text, model_name, max_le
     return train_text_tokens, val_text_tokens, test_text_tokens
 
 
-def convert_data_to_tensors(X_train, X_val, X_test, y_train, y_val, y_test, batch):
+def convert_data_to_tensors(X_train_input, X_val_input, X_train_mask, X_val_mask, y_train, y_val, batch):
     train_ds = tf.data.Dataset.from_tensor_slices(
-        (dict(X_train), tf.convert_to_tensor(y_train))).shuffle(1000).batch(batch)
+        ((X_train_input, X_train_mask), tf.convert_to_tensor(y_train))).shuffle(1000).batch(batch)
     val_ds = tf.data.Dataset.from_tensor_slices(
-        (dict(X_val), tf.convert_to_tensor(y_val))).batch(batch)
-    test_ds = tf.data.Dataset.from_tensor_slices(
-        (dict(X_test), tf.convert_to_tensor(y_test))).batch(batch)
-    return train_ds, val_ds, test_ds
+        ((X_val_input, X_val_mask), tf.convert_to_tensor(y_val))).batch(batch)
+    return train_ds, val_ds
 
 
 def bert_model(model_name, num_labels, optimizer, loss):
@@ -61,11 +59,11 @@ def emotion_model(model_name, max_length, optimizer, loss):
     outputs = bert_layer(input_ids=emotion_text_ids,
                          attention_mask=emotion_text_masks)
     output = outputs[1]
-    connected_layer = keras.layers.Dense(128, activation="relu")(output)
-    output_regressor = keras.layers.Dense(
-        6, activation="softmax")(connected_layer)
+    # connected_layer = keras.layers.Dense(128, activation="relu")(output)
+    output_emotion = keras.layers.Dense(
+        6, activation="softmax")(output)
     emotion_model = keras.models.Model(
-        inputs=[emotion_text_ids, emotion_text_masks], outputs=output_regressor)
+        inputs=[emotion_text_ids, emotion_text_masks], outputs=output_emotion)
 
     emotion_model.compile(
         optimizer=optimizer,
@@ -76,38 +74,87 @@ def emotion_model(model_name, max_length, optimizer, loss):
     return emotion_model
 
 
-stop_words = set(stopwords.words("english"))
-LR = 0.00002
-OPTIMIZER = keras.optimizers.Adam(learning_rate=LR)
-LOSS = "sparse_categorical_crossentropy"
-BATCH = 64
-EPOCHS = 5
-MODEL_NAME = "bert-base-uncased"
-MAX_LENGTH = 128
-NUM_LABELS = 6
+if __name__ == "__main__":
+    training = False
+    LR = 0.00002
+    OPTIMIZER = keras.optimizers.Adam(learning_rate=LR)
+    LOSS = "sparse_categorical_crossentropy"
+    BATCH = 64
+    EPOCHS = 5
+    MODEL_NAME = "bert-base-uncased"
+    MAX_LENGTH = 128
+    NUM_LABELS = 6
+    if training == True:
+        modelCheckpoint = keras.callbacks.ModelCheckpoint(
+            "backend\textEmotion\checkpoint\model.h5", monitor="val_loss", save_best_only=True, mode="min")
 
-df = pd.read_csv(
-    r"C:\Users\JohCa\Documents\GitHub\Revivo\backend\preprocessing\processed\dair-ai-emotion.csv")
-print(df)
-df = label_encoding(df=df)
-print(df)
-print("TRAIN VAL TEST SPLIT")
-train_features, val_features, test_features, train_labels, val_labels, test_labels = train_val_test_split(
-    df=df)
-print("train features: ", len(train_features))
-print("val features: ", len(val_features))
-print("test features: ", len(test_features))
-print("TRAIN VAL TEST TOKENIZE")
-train_tokens, val_tokens, test_tokens = tokenize_sentences(
-    train_features, val_features, test_features, MODEL_NAME, MAX_LENGTH)
-print(train_tokens[5])
-exit()
-print("TRAIN VAL TEST TENSORS")
-train_set, val_set, test_set = convert_data_to_tensors(
-    train_tokens, val_tokens, test_tokens, train_labels, val_labels, test_labels, BATCH)
-print("BERT MODEL")
-emotionClassifier = emotion_model(MODEL_NAME, MAX_LENGTH, OPTIMIZER, LOSS)
-print("TRAINING...")
-history = emotionClassifier.fit(
-    x=train_set, epochs=EPOCHS, validation_data=val_set)
-print(history)
+        df = pd.read_csv(
+            "backend\preprocessing\processed\dair-ai-emotion.csv")
+        df = label_encoding(df=df.head(15000))
+        train_features, val_features, test_features, train_labels, val_labels, test_labels = train_val_test_split(
+            df=df)
+        print(len(train_features), len(val_features), len(test_features))
+        train_tokens, val_tokens, test_tokens = tokenize_sentences(
+            train_features, val_features, test_features, MODEL_NAME, MAX_LENGTH)
+        train_set, val_set = convert_data_to_tensors(
+            train_tokens["input_ids"], val_tokens["input_ids"], train_tokens["attention_mask"], val_tokens["attention_mask"], train_labels, val_labels, BATCH)
+        train_set = train_set.prefetch(tf.data.AUTOTUNE)
+        train_set = train_set.cache()
+        val_set = val_set.prefetch(tf.data.AUTOTUNE)
+        val_set = val_set.cache()
+        emotionClassifier = emotion_model(
+            MODEL_NAME, MAX_LENGTH, OPTIMIZER, LOSS)
+        print("TRAINING...")
+        history = emotionClassifier.fit(
+            x=train_set, epochs=EPOCHS, validation_data=(val_set), callbacks=[modelCheckpoint])
+
+        loss_values = history.history["loss"]
+        val_loss_values = history.history["val_loss"]
+        acc_values = history.history["accuracy"]
+        val_acc_values = history.history["val_accuracy"]
+
+        plt.figure(figsize=(12, 8))
+        plt.plot(history.epoch, loss_values, label="TRAIN LOSS")
+        plt.plot(history.epoch, val_loss_values, label="VAL LOSS")
+        plt.legend(loc="upper right")
+        plt.xlabel("EPOCHS")
+        plt.ylabel("LOSS")
+        plt.show()
+
+        plt.figure(figsize=(12, 8))
+        plt.plot(history.epoch, acc_values, label="TRAIN ACCURACY")
+        plt.plot(history.epoch, val_acc_values, label="VAL ACCURACY")
+        plt.legend(loc="upper right")
+        plt.xlabel("EPOCHS")
+        plt.ylabel("ACCURACY")
+        plt.show()
+
+        test_set = tf.data.Dataset.from_tensor_slices(
+            ((test_tokens["input_ids"], test_tokens["attention_mask"]), tf.convert_to_tensor(test_labels))).batch(BATCH)
+        test_set = test_set.prefetch(tf.data.AUTOTUNE)
+        test_set = test_set.cache()
+
+        loss, acc = emotionClassifier.evaluate(test_set)
+        print(loss)
+        print(acc)
+    else:
+        df = pd.read_csv(
+            "backend\preprocessing\processed\tweet_emotions.csv")
+        df = df["text"].head(5)
+        print(df)
+        # exit()
+        sentence_tokenizer = transformers.BertTokenizer.from_pretrained(
+            MODEL_NAME)
+        text_tokens = sentence_tokenizer(
+            df.tolist(), add_special_tokens=True, padding="max_length", truncation=True, max_length=MAX_LENGTH, return_tensors="tf")
+        emotionClassifier = keras.models.load_model(
+            "/checkpoint/model.h5", custom_objects={"TFBertModel": transformers.TFBertModel})
+        test_ds = [np.array(text_tokens["input_ids"]),
+                   np.array(text_tokens["attention_mask"])]
+        preds = emotionClassifier.predict(test_ds)
+        print(preds)
+        predicted_labels = np.argmax(preds.tolist(), axis=1)
+        print(predicted_labels)
+        classes = {1: "fear", 2: "joy", 3: "love", 4: "sadness"}
+        categorical_value = [classes[label] for label in predicted_labels]
+        print(categorical_value)
