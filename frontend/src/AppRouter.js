7ff38e438642777a489992949458f11b941b87e2
Johann Cardona <2007048@brunel.ac.uk>
2024-12-16 22:17:56 +0000

    Update AppRouter.js

--- frontend/src/AppRouter.js
+++ frontend/src/AppRouter.js
@@ -1,9 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
-import RegisterLogin from "./components/login/RegisterLogin";
 import PersonalStats from "./components/profile/PersonalStats";
-import ArtStyling from "./components/moodscan/ArtStyling";
 import ChatbotInterface from "./components/chatbot/ChatbotInterface";
 
 function AppRouter() {
@@ -12,10 +10,8 @@ function AppRouter() {
       <Router>
         <Routes>
           <Route path="/" element={<App />}>
-            <Route index element={<RegisterLogin />} />
+            <Route index element={<ChatbotInterface />} />
             <Route path="/dashboard" element={<PersonalStats />} />
-            <Route path="/chat" element={<ChatbotInterface />} />
-            <Route path="/art_styling" element={<ArtStyling />} />
           </Route>
         </Routes>
       </Router>
