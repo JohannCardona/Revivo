0db08cd9ff98e93d550cf0263f9ae157a3a5e8ad
Johann Cardona <2007048@brunel.ac.uk>
2025-01-19 23:50:15 +0000

    Update MoodMain.jsx

--- frontend/src/components/profile/MoodMain.jsx
+++ frontend/src/components/profile/MoodMain.jsx
@@ -1,18 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import MoodGraph from "../profile/MoodGraph";
 import MoodTracker from "./MoodTracker";
 import axios from "axios";
 
 function MoodMain() {
-  const [userMoods, setUserMoods] = useState([]);
-
   const storeUserMoods = async (userMoods) => {
     axios.post("http://localhost:5000/store_user_moods", userMoods);
   };
 
   return (
     <div>
-      <h2>Mood Tracker</h2>
+      <h2 style={{ borderBottom: "2px solid black", width: 160, marginTop: 20 }}>
+        Mood Tracker
+      </h2>
       <MoodTracker addUserMood={storeUserMoods} />
       <MoodGraph />
     </div>
