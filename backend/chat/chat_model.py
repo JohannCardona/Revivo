0cdd2c5ba4b629859aca5b8a9b8c20d215419161
Johann Cardona <2007048@brunel.ac.uk>
2025-02-03 17:39:31 +0000

    Update chat_model.py

--- backend/chat/chat_model.py
+++ backend/chat/chat_model.py
@@ -1,14 +1,18 @@
+# import logs
 from flask import Blueprint, request, jsonify
 from http import HTTPStatus
-import os
-import requests
-from chat.model import chat_model
+# from chat.model import chat_model
+from chat.mental_health_fine_tuned_model import chat_model
+import time
 
 chat = Blueprint("chat", __name__)
 
 
 @chat.route("/chat", methods=["POST"])
 def chatbot_conversations():
+    start = time.time()
     data = request.get_json()
     bot_response = chat_model(data["prompt"])
+    end = time.time()
+    print(f"Execution lasted: {end- start} sec")
     return jsonify({"result": bot_response}), HTTPStatus.OK
