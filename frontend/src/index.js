c6b72c9466e12e61c6836f195c58d0ad6b4edc6d
Johann Cardona <2007048@brunel.ac.uk>
2025-02-08 18:40:00 +0000

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,9 +6,9 @@ import { ThemeProvider } from "./components/Theme/Theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <ThemeProvider>
       <AppRouter />
     </ThemeProvider>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
