601ac01e3f0d9dab1e37a8d7b73c35ac3c3907a5
Johann Cardona <2007048@brunel.ac.uk>
2025-02-09 14:09:59 +0000

    Update chatgpt_api.py

--- backend/chat/chatgpt_api.py
+++ backend/chat/chatgpt_api.py
@@ -1,8 +1,11 @@
 import openai
 import os
 from dotenv import load_dotenv
+from flask import jsonify, Blueprint
+from http import HTTPStatus
 
-load_dotenv
+chatgpt = Blueprint("chatgpt", __name__)
+load_dotenv()
 
 
 def initialise_API():
@@ -13,14 +16,30 @@ def initialise_API():
     return openai
 
 
+@chatgpt.route("/fetch_gpt_bot_response", methods=["POST"])
 def get_chatgpt_api_response(prompt: str):
     system = "You are a highly professional and empathetic AI assistant designed to provide personalised support in 1-to-1 conversations. Your role is to assist university students with their questions, offer guidance effectively, and solve problems. Maintain friendly, helpful, and understanding tone while respecting the user's privacy and boundaries. Tailor your responses the user's context, and avoid giving unhelpful or generic answers. If your unable to assist provide clear explanations and suggest alternative resources/solutions. Do not provide legal, medical, financial advice unless trained for those domains, and always disclaim appropriately in such cases."
 
     client = initialise_API()
     LLM_response = client.chat.completions.create(
-        model="gpt4-o",
-        message=[{"role": "system", "content": system},
-                 {"role": "user", "content": prompt}],
+        model="gpt-4o",
+        messages=[{"role": "system", "content": system},
+                  {"role": "user", "content": prompt}],
         temperature=1,
     )
-    return LLM_response
+    return jsonify({"result": LLM_response}), HTTPStatus.OK
+
+
+def get_chatgpt_bot_response(prompt):
+    system = "You are a highly professional and empathetic AI assistant designed to provide personalised support in 1-to-1 conversations. Your role is to assist university students with their questions, offer guidance effectively, and solve problems. Maintain friendly, helpful, and understanding tone while respecting the user's privacy and boundaries. Tailor your responses the user's context, and avoid giving unhelpful or generic answers. If your unable to assist provide clear explanations and suggest alternative resources/solutions. Do not provide legal, medical, financial advice unless trained for those domains, and always disclaim appropriately in such cases."
+    user_prompt = f"Provide the completed version of the response. Keep it concise:\n\n{prompt}"
+
+    client = initialise_API()
+    LLM_response = client.chat.completions.create(
+        model="gpt-4o-mini-2024-07-18",
+        messages=[{"role": "developer", "content": system},
+                  {"role": "user", "content": user_prompt}],
+        temperature=0.5,
+    )
+    final_response = prompt + " " + LLM_response.choices[0].message.content
+    return final_response
