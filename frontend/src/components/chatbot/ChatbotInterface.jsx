e2711811d819c50e36c2c53fc447ab27a2c1b35b
Johann Cardona <2007048@brunel.ac.uk>
2025-02-09 22:35:48 +0000

    Fixing issue with incorrect image displayed on modal

--- frontend/src/components/chatbot/ChatbotInterface.jsx
+++ frontend/src/components/chatbot/ChatbotInterface.jsx
@@ -715,31 +715,40 @@ function ChatbotInterface() {
                     />
                   ) : conversation.chatbot &&
                     conversation.response.includes("data:image") ? (
-                    <div key={conversation.id}>
+                    <div key={conversation.id} id={conversation.id}>
                       <img
+                        id={conversation.id}
                         src={conversation.response}
-                        onClick={() => handleImageModal(conversation.response)}
+                        onClick={() => setClickedImage(conversation.response)}
                         alt="chat icon"
                       />
                       {clickedImage && (
-                        <div className="modal-container">
+                        <div
+                          key={conversation.id}
+                          id={conversation.id}
+                          className="modal-container"
+                        >
                           <div className="modal-image">
                             <CloseIcon
                               sx={{ fontSize: 40 }}
                               onClick={() => modalClose(conversation.response)}
                             />
                           </div>
-                          <div className="download-container">
+                          <div
+                            key={conversation.id}
+                            id={conversation.id}
+                            className="download-container"
+                          >
                             <button
                               onClick={() =>
-                                downloadDALLEImage(conversation.response)
+                                downloadDALLEImage(clickedImage)
                               }
                             >
                               <IoCloudDownloadOutline sx={{ fontSize: 50 }} />
                             </button>
                             <img
-                              key={conversation.id}
-                              src={conversation.response}
+                              id={conversation.id}
+                              src={clickedImage}
                               alt="enlarged"
                             />
                           </div>
