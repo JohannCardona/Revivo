cbf701775bc8a4c7be44f8883e896eef576f6ab0
Johann Cardona <2007048@brunel.ac.uk>
2025-02-15 21:46:33 +0000

    Update storeVideoCollection.py

--- backend/videos/storeVideoCollection.py
+++ backend/videos/storeVideoCollection.py
@@ -5,12 +5,12 @@ mentalHealthVideos = [
         "category": "Emotional regulation",
         "videos": [
             {
-                "video_title": "Managing Anxiety",
-                "video_url": "https://www.youtube.com/embed/DZqkeMoQA38",
+                "video_title": "How Healthy People Regulate Their Emotions",
+                "video_url": "https://www.youtube.com/embed/1DmphC3Wozo",
             },
             {
-                "video_title": "Breathing Exercises for Anxiety",
-                "video_url": "https://www.youtube.com/embed/DZqkeMoQA38",
+                "video_title": "Steps to Improve Your Emotional Intelligence",
+                "video_url": "https://www.youtube.com/embed/D6_J7FfgWVc",
             },
         ],
     },
@@ -18,12 +18,12 @@ mentalHealthVideos = [
         "category": "Coping mechanisms",
         "videos": [
             {
-                "video_title": "Understanding Depression",
-                "video_url": "https://www.youtube.com/embed/DZqkeMoQA38",
+                "video_title": "Are Your Coping Mechanisms Healthy",
+                "video_url": "https://www.youtube.com/embed/XTlDS7ju_28",
             },
             {
-                "video_title": "Overcoming Negative Thoughts",
-                "video_url": "https://www.youtube.com/embed/DZqkeMoQA38",
+                "video_title": "Copying Mechanisms You Shouldn't Ignore",
+                "video_url": "https://www.youtube.com/embed/PyIyxeMXKeg",
             },
         ],
     },
@@ -31,12 +31,12 @@ mentalHealthVideos = [
         "category": "Mindfulness and meditation",
         "videos": [
             {
-                "video_title": "Stress Relief Techniques",
-                "video_url": "https://www.youtube.com/embed/DZqkeMoQA38",
+                "video_title": "5-Minute Meditation You Can Do Anywhere",
+                "video_url": "https://www.youtube.com/embed/inpok4MKVLM",
             },
             {
-                "video_title": "Mindful Stress Management",
-                "video_url": "https://www.youtube.com/embed/DZqkeMoQA38",
+                "video_title": "How Mindfulness Changes The Emotional Life of Our Brains",
+                "video_url": "https://www.youtube.com/embed/7CBfCW67xT8",
             },
         ],
     },
@@ -44,12 +44,12 @@ mentalHealthVideos = [
         "category": "Resilience building",
         "videos": [
             {
-                "video_title": "Managing Anxiety",
-                "video_url": "https://www.youtube.com/embed/DZqkeMoQA38",
+                "video_title": "Building Resilience",
+                "video_url": "https://www.youtube.com/embed/GLAdRgft7pU",
             },
             {
-                "video_title": "Breathing Exercises for Anxiety",
-                "video_url": "https://www.youtube.com/embed/DZqkeMoQA38",
+                "video_title": "8 Things Resilient ",
+                "video_url": "https://www.youtube.com/embed/RJKbr8VvvbY",
             },
         ],
     },
@@ -57,12 +57,12 @@ mentalHealthVideos = [
         "category": "Anger management",
         "videos": [
             {
-                "video_title": "Understanding Depression",
-                "video_url": "https://www.youtube.com/embed/DZqkeMoQA38",
+                "video_title": "Anger Management",
+                "video_url": "https://www.youtube.com/embed/a8t5SwmqDnk",
             },
             {
-                "video_title": "Overcoming Negative Thoughts",
-                "video_url": "https://www.youtube.com/embed/DZqkeMoQA38",
+                "video_title": "The Antidote to Anger",
+                "video_url": "https://www.youtube.com/embed/hCIfi-xvjgE",
             },
         ],
     },
@@ -70,12 +70,12 @@ mentalHealthVideos = [
         "category": "Social anxiety",
         "videos": [
             {
-                "video_title": "Stress Relief Techniques",
-                "video_url": "https://www.youtube.com/embed/DZqkeMoQA38",
+                "video_title": "Social Anxiety in the Modern World",
+                "video_url": "https://www.youtube.com/embed/EFhP4wP1TzU",
             },
             {
-                "video_title": "Mindful Stress Management",
-                "video_url": "https://www.youtube.com/embed/DZqkeMoQA38",
+                "video_title": "How to Overcome Social Anxiety",
+                "video_url": "https://www.youtube.com/embed/X_ZKkvhXNJk",
             },
         ],
     },
@@ -83,12 +83,12 @@ mentalHealthVideos = [
         "category": "Relationship issues",
         "videos": [
             {
-                "video_title": "Managing Anxiety",
-                "video_url": "https://www.youtube.com/embed/DZqkeMoQA38",
+                "video_title": "Incompatible Relationship",
+                "video_url": "https://www.youtube.com/embed/qadSvKa2eHY",
             },
             {
-                "video_title": "Breathing Exercises for Anxiety",
-                "video_url": "https://www.youtube.com/embed/DZqkeMoQA38",
+                "video_title": "Trauma and The Impact in Relationships",
+                "video_url": "https://www.youtube.com/embed/O4iJeAGuhRA",
             },
         ],
     },
@@ -96,12 +96,12 @@ mentalHealthVideos = [
         "category": "Loneliness and isolation",
         "videos": [
             {
-                "video_title": "Understanding Depression",
-                "video_url": "https://www.youtube.com/embed/DZqkeMoQA38",
+                "video_title": "How Isolation Affects You",
+                "video_url": "https://www.youtube.com/embed/c7_qmkmP-JM",
             },
             {
-                "video_title": "Overcoming Negative Thoughts",
-                "video_url": "https://www.youtube.com/embed/DZqkeMoQA38",
+                "video_title": "Why You Might Feel Lonely",
+                "video_url": "https://www.youtube.com/embed/nSVR5AoWUcU",
             },
         ],
     },
@@ -109,25 +109,25 @@ mentalHealthVideos = [
         "category": "Healthy boundaries",
         "videos": [
             {
-                "video_title": "Stress Relief Techniques",
-                "video_url": "https://www.youtube.com/embed/DZqkeMoQA38",
+                "video_title": "Good Boundaries Free You",
+                "video_url": "https://www.youtube.com/embed/rtsHUeKnkC8",
             },
             {
-                "video_title": "Mindful Stress Management",
-                "video_url": "https://www.youtube.com/embed/DZqkeMoQA38",
+                "video_title": "Set Healthy Boundaries with Toxic People",
+                "video_url": "https://www.youtube.com/embed/A-CqifOE0Nw",
             },
         ],
     },
     {
-        "category": "Peer pressure and bullying",
+        "category": "Social pressure",
         "videos": [
             {
-                "video_title": "Managing Anxiety",
-                "video_url": "https://www.youtube.com/embed/DZqkeMoQA38",
+                "video_title": "Playing the Game of Social Pressure",
+                "video_url": "https://www.youtube.com/embed/ZskcFSsbi9E",
             },
             {
-                "video_title": "Breathing Exercises for Anxiety",
-                "video_url": "https://www.youtube.com/embed/DZqkeMoQA38",
+                "video_title": "Peer Pressure or Peer Support",
+                "video_url": "https://www.youtube.com/embed/j1Fj88B46F8",
             },
         ],
     },
