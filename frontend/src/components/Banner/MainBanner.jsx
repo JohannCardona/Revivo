2fd6f9ef00f655db585b478a0c5531c128d35c45
Johann Cardona <2007048@brunel.ac.uk>
2025-02-21 12:08:05 +0000

    Create MainBanner.jsx

---
+++ frontend/src/components/Banner/MainBanner.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../../styles/banner/banner.css";
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <h1>Welcome to Revivo</h1>
+      <p>
+        Your companion for mental well-being. Chat with our support bot, track
+        your mood and personal stats and explore audios, videos, and advice to help you feel
+        better.
+      </p>
+    </div>
+  );
+};
+
+export default Banner;
