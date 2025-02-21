dc9e27624602b5baf3a27e7ce368cc42e4080185
Johann Cardona <2007048@brunel.ac.uk>
2025-02-21 12:08:17 +0000

    Update ExistingUser.jsx

--- frontend/src/components/login/ExistingUser.jsx
+++ frontend/src/components/login/ExistingUser.jsx
@@ -5,6 +5,7 @@ import "../../styles/login/RegisterLogin.css";
 import Swal from "sweetalert2";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import Banner from "../Banner/MainBanner";
 
 function ExistingUser() {
   const [existingUser, setExistingUser] = useState("");
@@ -71,67 +72,70 @@ function ExistingUser() {
   };
 
   return (
-    <div className="registerlogin-container">
-      <div className="account-forms">
-        <div className="new">
-          {/* Form that contains sign in field */}
-          <form className="login" noValidate>
-            <h2 style={{ color: "var(--text)" }} className="form-title">
-              Sign in
-            </h2>
-            <div className="input">
+    <>
+      <Banner />
+      <div className="registerlogin-container">
+        <div className="account-forms">
+          <div className="new">
+            {/* Form that contains sign in field */}
+            <form className="login" noValidate>
+              <h2 style={{ color: "var(--text)" }} className="form-title">
+                Sign in
+              </h2>
+              <div className="input">
+                <input
+                  type="text"
+                  className="username"
+                  value={existingUser}
+                  onChange={(e) => setExistingUser(e.target.value)}
+                  placeholder="Enter your username"
+                />
+              </div>
+              {/* Redirect to login form */}
               <input
-                type="text"
-                className="username"
-                value={existingUser}
-                onChange={(e) => setExistingUser(e.target.value)}
-                placeholder="Enter your username"
+                value="Sign in"
+                type="submit"
+                className="mood-submit"
+                onClick={handleUserSignIn}
               />
-            </div>
-            {/* Redirect to login form */}
-            <input
-              value="Sign in"
-              type="submit"
-              className="mood-submit"
-              onClick={handleUserSignIn}
-            />
-          </form>
-        </div>
-      </div>
-
-      <div className="new-panels-container">
-        <div className="new-panel new-left-panel">
-          <div className="new-panel-content">
-            <h3>Good to see you again!</h3>
-            <p>Enter your username to connect with your chatbot.</p>
-            <p
-              style={{
-                fontWeight: "bold",
-                fontStyle: "italic",
-                fontSize: 16,
-                color: "white",
-              }}
-            >
-              Click here if you are a new user?{" "}
-              <button
-                style={{ marginLeft: 20 }}
-                className="image-button"
-                id="sign-up-btn"
-                onClick={goBackToRegister}
-              >
-                Sign up
-              </button>
-            </p>
+            </form>
           </div>
-          <img
-            style={{ width: "100%", marginTop: 1 }}
-            src={login}
-            className="image"
-            alt="sign in logo"
-          />
+        </div>
+
+        <div className="new-panels-container">
+          <div className="new-panel new-left-panel">
+            <div className="new-panel-content">
+              <h3>Good to see you again!</h3>
+              <p>Enter your username to connect with your chatbot.</p>
+              <p
+                style={{
+                  fontWeight: "bold",
+                  fontStyle: "italic",
+                  fontSize: 16,
+                  color: "white",
+                }}
+              >
+                Click here if you are a new user?{" "}
+                <button
+                  style={{ marginLeft: 20 }}
+                  className="image-button"
+                  id="sign-up-btn"
+                  onClick={goBackToRegister}
+                >
+                  Sign up
+                </button>
+              </p>
+            </div>
+            <img
+              style={{ width: "100%", marginTop: 1 }}
+              src={login}
+              className="image"
+              alt="sign in logo"
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
