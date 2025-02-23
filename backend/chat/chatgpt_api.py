1858a6a3c5032e7bed4ca07fad188a1f04b0fb67
Johann Cardona <2007048@brunel.ac.uk>
2025-02-23 19:33:42 +0000

    Update chatgpt_api.py

--- backend/chat/chatgpt_api.py
+++ backend/chat/chatgpt_api.py
@@ -1,10 +1,7 @@
 import openai
 import os
 from dotenv import load_dotenv
-# from flask import jsonify, Blueprint
-# from http import HTTPStatus
 
-# chatgpt = Blueprint("chatgpt", __name__)
 load_dotenv()
 
 
@@ -14,20 +11,6 @@ def initialise_API():
         raise ValueError("API key must be set in environment")
     openai.api_key = api_key
     return openai
-
-
-# @chatgpt.route("/fetch_gpt_bot_response", methods=["POST"])
-# def get_chatgpt_api_response(prompt: str):
-#     system = "You are a highly professional and empathetic AI assistant designed to provide personalised support in 1-to-1 conversations. Your role is to assist university students with their questions, offer guidance effectively, and solve problems. Maintain friendly, helpful, and understanding tone while respecting the user's privacy and boundaries. Tailor your responses the user's context, and avoid giving unhelpful or generic answers. If your unable to assist provide clear explanations and suggest alternative resources/solutions. Do not provide legal, medical, financial advice unless trained for those domains, and always disclaim appropriately in such cases."
-
-#     client = initialise_API()
-#     LLM_response = client.chat.completions.create(
-#         model="gpt-4o",
-#         messages=[{"role": "system", "content": system},
-#                   {"role": "user", "content": prompt}],
-#         temperature=1,
-#     )
-#     return jsonify({"result": LLM_response}), HTTPStatus.OK
 
 
 def get_chatgpt_bot_response(prompt):
@@ -41,5 +24,5 @@ def get_chatgpt_bot_response(prompt):
                   {"role": "user", "content": user_prompt}],
         temperature=0.5,
     )
-    final_response = prompt + " " + LLM_response.choices[0].message.content
+    final_response = LLM_response.choices[0].message.content
     return final_response
