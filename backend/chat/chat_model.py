5ce1785f5fbd500f1a412529479b8962e3d79976
Johann Cardona <2007048@brunel.ac.uk>
2025-03-30 14:58:21 +0100

    Update chat_model.py

--- backend/chat/chat_model.py
+++ backend/chat/chat_model.py
@@ -9,18 +9,13 @@ chat = Blueprint("chat", __name__)
 
 @chat.route("/chat", methods=["POST"])
 def chatbot_conversations():
-    start = time.time()
     data = request.get_json()
     punct_marks = {".", "!", "?"}
     response = ""
     bot_response = chat_model(data["prompt"])
     if bot_response.strip()[-1] not in punct_marks:
-        print("SENTENCE INCOMPLETE")
         gpt_response = get_chatgpt_bot_response(prompt=data["prompt"])
         response = gpt_response
     else:
-        print("COMPLETE SENTENCE")
         response = bot_response
-    end = time.time()
-    print(f"Execution lasted: {end- start} sec")
     return jsonify({"result": response}), HTTPStatus.OK
