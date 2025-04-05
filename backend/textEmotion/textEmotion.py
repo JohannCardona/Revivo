966a85ea994a00f301759397344e9191e3af9bbf
Johann Cardona <2007048@brunel.ac.uk>
2025-04-05 12:24:24 +0100

    Added header token to text emotion classification

--- backend/textEmotion/textEmotion.py
+++ backend/textEmotion/textEmotion.py
@@ -1,9 +1,9 @@
 import transformers
 import numpy as np
 import keras
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from http import HTTPStatus
-from time import time
+import jwt
 from transformers.optimization_tf import WarmUp
 
 emotion_classifier = Blueprint("emotion_classifier", __name__)
@@ -25,14 +25,19 @@ def fetch_emotion_from_text(prompt: str):
     Returns:
         str: emotion tag
     """
-    sentence_tokenizer = transformers.AutoTokenizer.from_pretrained(MODEL_NAME)
-    text_tokens = sentence_tokenizer(prompt, add_special_tokens=True, padding="max_length",
-                                     truncation=True, max_length=MAX_LENGTH, return_tensors="tf")
-    emotionClassifier = keras.models.load_model(r"\Revivo\backend\textEmotion\checkpoint\tiny_bert.h5", custom_objects={
-                                                "TFBertModel": transformers.TFBertModel})
-    user_input = [np.array(text_tokens["input_ids"]),
-                  np.array(text_tokens["attention_mask"])]
-    preds = emotionClassifier.predict(user_input)
-    predicted_labels = np.argmax(preds.tolist(), axis=1)
-    categorical_value = [classes[label] for label in predicted_labels]
+    jwt_token = request.authorization
+    token = jwt_token.token
+    categorical_value = ""
+    decoded_token = jwt.decode(token, key="revivo", algorithms=["HS256"])
+    if decoded_token:
+        sentence_tokenizer = transformers.AutoTokenizer.from_pretrained(MODEL_NAME)
+        text_tokens = sentence_tokenizer(prompt, add_special_tokens=True, padding="max_length",
+                                        truncation=True, max_length=MAX_LENGTH, return_tensors="tf")
+        emotionClassifier = keras.models.load_model(r"C:\Users\JohCa\Documents\GitHub\Revivo\backend\textEmotion\checkpoint\tiny_bert_connect_drop2.h5", custom_objects={
+                                                    "TFBertModel": transformers.TFBertModel, "WarmUp": WarmUp})
+        user_input = [np.array(text_tokens["input_ids"]),
+                    np.array(text_tokens["attention_mask"])]
+        preds = emotionClassifier.predict(user_input)
+        predicted_labels = np.argmax(preds.tolist(), axis=1)
+        categorical_value = [classes[label] for label in predicted_labels]
     return jsonify(categorical_value), HTTPStatus.OK
