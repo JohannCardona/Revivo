ce319c3a957844cbe022cf3134a2a20f626cc700
Johann Cardona <2007048@brunel.ac.uk>
2025-04-05 15:15:01 +0100

    Update ChatbotInterface.jsx

--- frontend/src/components/chatbot/ChatbotInterface.jsx
+++ frontend/src/components/chatbot/ChatbotInterface.jsx
@@ -1,13 +1,9 @@
 import React, { useState, useRef, useEffect } from "react";
-import { Button, Tooltip } from "@mui/material";
-import SendRoundedIcon from "@mui/icons-material/SendRounded";
 import { Comment } from "react-loader-spinner";
 import chaticon from "../../images/chatbot.svg";
-import CloseIcon from "@mui/icons-material/Close";
 import "../../styles/chatbot/ChatbotInterface.css";
 import Swal from "sweetalert2";
 import Banner from "../Banner/Banner";
-import { IoCloudDownloadOutline } from "react-icons/io5";
 import {
   genreEmotions,
   recommend_songs,
@@ -15,11 +11,7 @@ import {
   fetching_recommending_songs_response,
   fetch_song_genre_selection,
 } from "./music/MusicRecommendations";
-import {
-  generate_image,
-  downloadDALLEImage,
-  displayImages,
-} from "./images/ArtisticImages";
+import { generate_image, downloadDALLEImage } from "./images/ArtisticImages";
 import { fetch_emotion_from_text } from "./emotions/Emotions";
 import {
   handleChatbotResponse,
@@ -36,6 +28,7 @@ import { dynamic_mood_tracking } from "./moods/FetchMoods";
 import Sidebar from "./Sidebar";
 import DisplayImages from "./images/DisplayImages";
 import Spotify from "./music/Spotify";
+import UserInputs from "./music/UserInputs";
 
 function ChatbotInterface() {
   let [prompt, setPrompt] = useState("");
@@ -599,38 +592,10 @@ function ChatbotInterface() {
             </>
           )}
         </div>
-        <div className="item-2">
-          <div className="user-item user-text-container">
-            <textarea
-              rows="1"
-              placeholder="Message Revivo bot"
-              className="text-box"
-              value={prompt}
-              style={{ resize: "none" }}
-              onChange={(e) => setPrompt(e.target.value)}
-              onKeyDown={(e) => {
-                if (e.key === "Enter") handleConversationSubmit(e);
-              }}
-            />
-          </div>
-          <div className="user-item mic"></div>
-          <div className="user-item">
-            <Tooltip title="Send">
-              <Button
-                style={{
-                  backgroundColor: "var(--image-button)",
-                  padding: "12px 10px",
-                  borderRadius: 8,
-                  paddingLeft: 3,
-                  marginRight: 8,
-                }}
-                variant="contained"
-                endIcon={<SendRoundedIcon style={{ color: "var(--text)" }} />}
-                onClick={handleConversationSubmit}
-              ></Button>
-            </Tooltip>
-          </div>
-        </div>
+        <UserInputs
+          setPrompt={setPrompt}
+          handleConversationSubmit={handleConversationSubmit}
+        />
       </div>
     </div>
   );
