0c77b9e755770bf66bafebde89dba16309189b8d
Johann Cardona <2007048@brunel.ac.uk>
2025-04-05 12:23:45 +0100

    Update chatgpt_api.py

--- backend/chat/chatgpt_api.py
+++ backend/chat/chatgpt_api.py
@@ -15,7 +15,7 @@ def initialise_API():
 
 def get_chatgpt_bot_response(prompt):
     system = "You are a highly professional and empathetic AI assistant designed to provide personalised support in 1-to-1 conversations. Your role is to assist university students with their questions, offer guidance effectively, and solve problems. Maintain friendly, helpful, and understanding tone while respecting the user's privacy and boundaries. Tailor your responses the user's context, and avoid giving unhelpful or generic answers. If your unable to assist provide clear explanations and suggest alternative resources/solutions. Do not provide legal, medical, financial advice unless trained for those domains, and always disclaim appropriately in such cases."
-    user_prompt = f"Provide the completed version of the response. Keep it concise:\n\n{prompt}"
+    user_prompt = f"Keep it concise:\n\n{prompt}"
 
     client = initialise_API()
     LLM_response = client.chat.completions.create(
