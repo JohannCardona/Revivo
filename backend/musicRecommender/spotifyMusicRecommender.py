26ae64fd682a27ddd5e8a88bb02261d4a2eeac38
Johann Cardona <2007048@brunel.ac.uk>
2025-04-05 12:24:07 +0100

    Added header token to Spotify recommendation

--- backend/musicRecommender/spotifyMusicRecommender.py
+++ backend/musicRecommender/spotifyMusicRecommender.py
@@ -1,10 +1,11 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from http import HTTPStatus
 import base64
 import requests
 import os
 from dotenv import load_dotenv
 from random import randint
+import jwt
 
 load_dotenv()
 spotify_recommender = Blueprint("spotify_recommender", __name__)
@@ -35,21 +36,27 @@ def spotify_login_token():
 
 @spotify_recommender.route("/music_recommendations/<genre>", methods=['GET'])
 def music_recommendations(genre):
-    token = spotify_login_token()
-    if not token:
-        return jsonify({"error": "Failed to authenticate with Spotify"}), HTTPStatus.INTERNAL_SERVER_ERROR
-
-    headers = {"Authorization": f"Bearer {token[0]}"}
-    offset = randint(0, 500)
-    params = {"q": f"genre:{genre}", "type": "track", "limit": 5, "offset": offset}
-    res = requests.get(SEARCH_URL, headers=headers, params=params)
-
-    if res.status_code != 200:
-        return jsonify({"error": "Failed to authenticate with Spotify"}), res.status_code
-    music_info = res.json()
+    jwt_token = request.authorization
+    print("JWT TOKEN: ", jwt_token)
+    token = jwt_token.token
+    print("TOKEN: ", token)
+    decoded_token = jwt.decode(token, key="revivo", algorithms=["HS256"])
     recommended_songs = []
-    for song in music_info["tracks"]["items"]:
-        songs_info = {"name": song["name"], "artist": ", ".join(
-            [artist["name"] for artist in song["artists"]]), "album": song["album"]["name"], "albumDate": song["album"]["release_date"], "spotifyURL": song["external_urls"]["spotify"], "songId": song["id"]}
-        recommended_songs.append(songs_info)
+    if decoded_token:
+        spot_token = spotify_login_token()
+        if not spot_token:
+            return jsonify({"result": "Failed to authenticate with Spotify"}), HTTPStatus.INTERNAL_SERVER_ERROR
+
+        headers = {"Authorization": f"Bearer {spot_token[0]}"}
+        offset = randint(0, 500)
+        params = {"q": f"genre:{genre}", "type": "track", "limit": 5, "offset": offset}
+        res = requests.get(SEARCH_URL, headers=headers, params=params)
+
+        if res.status_code != 200:
+            return jsonify({"result": "Failed to authenticate with Spotify"}), res.status_code
+        music_info = res.json()
+        for song in music_info["tracks"]["items"]:
+            songs_info = {"name": song["name"], "artist": ", ".join(
+                [artist["name"] for artist in song["artists"]]), "album": song["album"]["name"], "albumDate": song["album"]["release_date"], "spotifyURL": song["external_urls"]["spotify"], "songId": song["id"]}
+            recommended_songs.append(songs_info)
     return jsonify(recommended_songs), HTTPStatus.OK
