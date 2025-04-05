83dc7db1523c9a43c7b71ad864d5442b78658c4a
Johann Cardona <2007048@brunel.ac.uk>
2025-04-05 12:23:36 +0100

    Added header token to chatbot response

--- backend/chat/chat_model.py
+++ backend/chat/chat_model.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify
 from http import HTTPStatus
 from chat.mental_health_fine_tuned_model import chat_model
-import time
+import jwt
 from chat.chatgpt_api import get_chatgpt_bot_response
 
 chat = Blueprint("chat", __name__)
@@ -9,13 +9,18 @@ chat = Blueprint("chat", __name__)
 
 @chat.route("/chat", methods=["POST"])
 def chatbot_conversations():
-    data = request.get_json()
-    punct_marks = {".", "!", "?"}
+    jwt_token = request.authorization
+    token = jwt_token.token
+    decoded_token = jwt.decode(token, key="revivo", algorithms=["HS256"])
     response = ""
-    bot_response = chat_model(data["prompt"])
-    if bot_response.strip()[-1] not in punct_marks:
-        gpt_response = get_chatgpt_bot_response(prompt=data["prompt"])
-        response = gpt_response
-    else:
-        response = bot_response
+    if decoded_token:
+        data = request.get_json()
+        punct_marks = {".", "!", "?"}
+        response = ""
+        bot_response = chat_model(data["prompt"])
+        if bot_response.strip()[-1] not in punct_marks:
+            gpt_response = get_chatgpt_bot_response(prompt=data["prompt"])
+            response = gpt_response
+        else:
+            response = bot_response
     return jsonify({"result": response}), HTTPStatus.OK
