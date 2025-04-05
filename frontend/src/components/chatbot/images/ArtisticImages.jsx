f0f88777fa494e9365fae7bad3e778756ed532ce
Johann Cardona <2007048@brunel.ac.uk>
2025-04-05 14:15:53 +0100

    Create ArtisticImages.jsx

---
+++ frontend/src/components/chatbot/images/ArtisticImages.jsx
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export const generate_image = async (prompt) => {
+    const response = await axios.post(
+      `http://localhost:5000/image_generation`,
+      { prompt },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      }
+    );
+    const genImage = `data:image/jpeg;base64,${response.data[0].result}`;
+    return genImage;
+  };
