25d99e2235bc38ddd5511a98542d15ecb190f964
Johann Cardona <2007048@brunel.ac.uk>
2025-04-06 20:13:31 +0100

    Update account_api.jsx

--- frontend/src/components/login/account_api.jsx
+++ frontend/src/components/login/account_api.jsx
@@ -10,7 +10,7 @@ const fireAlert = (response, type, color) => {
   });
 };
 
-const fireAlert1 = (response, type, color, navigateToLogin) => {
+const fireAlertSignUp = (response, type, color, navigateToLogin) => {
   Swal.fire({
     title: response,
     confirmButtonText: "OK",
@@ -21,7 +21,19 @@ const fireAlert1 = (response, type, color, navigateToLogin) => {
   });
 };
 
+const fireAlertSignin = (response, type, color, navigateToChat) => {
+  Swal.fire({
+    title: response,
+    confirmButtonText: "OK",
+    confirmButtonColor: color,
+    icon: type,
+  }).then(() => {
+    navigateToChat();
+  });
+};
+
 export const handleUserRegister = (newUser, navigateToLogin) => {
+  // e.preventDefault();
   if (newUser.trim() === "") {
     fireAlert("Username must not be empty", "error", "red");
     return;
@@ -32,12 +44,48 @@ export const handleUserRegister = (newUser, navigateToLogin) => {
     })
     .then((response) => {
       if (response.status === 201) {
-        fireAlert1(
+        fireAlertSignUp(
           "That was easy. Let's head to the login.",
           "success",
           "green",
           navigateToLogin
         );
+      }
+    })
+    .catch((error) => {
+      const error_msg = error.response?.data?.result || error.message;
+      Swal.fire({
+        title: error_msg,
+        confirmButtonText: "OK",
+        confirmButtonColor: "#ff0055",
+        icon: "error",
+      });
+    });
+};
+
+export const handleUserSignIn = (existingUser, navigateToChat) => {
+  // e.preventDefault();
+  if (existingUser.trim() === "") {
+    fireAlert("Username must not be empty", "error", "red");
+    return;
+  }
+  return axios
+    .post("http://localhost:5000/login", {
+      existingUser,
+    })
+    .then((response) => {
+      if (response.status === 200) {
+        const token = response.data.token;
+        localStorage.setItem("user", existingUser);
+        localStorage.setItem("token", token);
+        fireAlertSignin(
+          "You're in. Let's start the conversation.",
+          "success",
+          "green",
+          navigateToChat
+        );
+        console.log("Token:", token);
+        return token;
       }
     })
     .catch((error) => {
