f0c337389a22e3854f616e131897c15c1d744ce3
Johann Cardona <2007048@brunel.ac.uk>
2025-04-07 00:10:32 +0100

    Update MoodMain.jsx

--- frontend/src/components/MoodTracker/MoodMain.jsx
+++ frontend/src/components/MoodTracker/MoodMain.jsx
@@ -1,19 +1,10 @@
 import React from "react";
 import MoodGraph from "../MoodTracker/MoodGraph";
 import MoodTracker from "./MoodTracker";
-import axios from "axios";
 import "../../styles/mood/mood.css";
+import { storeUserMoods } from "./MoodAPI";
 
 function MoodMain() {
-  const storeUserMoods = async (userMoods) => {
-    axios.post("http://localhost:5000/store_user_moods", userMoods, {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${localStorage.getItem("token")}`,
-      },
-    });
-  };
-
   return (
     <div
       className="mood-container"
