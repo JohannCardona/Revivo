ecf6a9a5bcfa0d08218a3a4a783f646cde6ef694
Johann Cardona <2007048@brunel.ac.uk>
2025-04-06 20:13:43 +0100

    Update VideoMain.jsx

--- frontend/src/components/library/VideoMain.jsx
+++ frontend/src/components/library/VideoMain.jsx
@@ -1,24 +1,17 @@
 import React, { useEffect, useState } from "react";
 import VideoLibrary from "./VideoLibrary";
-import axios from "axios";
+import { fetchVideoCollection } from "./VideosApi";
 
 function VideoMain() {
   const [videos, setVideos] = useState([]);
 
+  const get_videos = async () => {
+    const fetched_videos = await fetchVideoCollection();
+    setVideos(fetched_videos.data);
+  }
+
   useEffect(() => {
-    const fetchVideoCollection = async () => {
-      await axios
-        .get("http://localhost:5000/videos", {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${localStorage.getItem("token")}`,
-          },
-        })
-        .then((response) => {
-          setVideos(response.data);
-        });
-    };
-    fetchVideoCollection();
+    get_videos();
   }, []);
 
   return (
