187e3540a9ec434b41e587e4adff8fddbbced63d
Johann Cardona <2007048@brunel.ac.uk>
2025-04-06 01:36:47 +0100

    Update UserConversations.jsx

--- frontend/src/components/chatbot/chats/UserConversations.jsx
+++ frontend/src/components/chatbot/chats/UserConversations.jsx
@@ -20,42 +20,6 @@ export const fetch_conversation_title = async (
     });
 };
 
-export const store_user_conversations = async (
-  conversations,
-  isNewChat,
-  conversationTitle,
-  setConversations
-) => {
-  const user = localStorage.getItem("user");
-  if (conversations.length !== 0) {
-    if (
-      localStorage.getItem("user_message").includes("bye") ||
-      localStorage.getItem("user_message").includes("exit") ||
-      localStorage.getItem("user_message").includes("bye bye") ||
-      localStorage.getItem("user_message").includes("goodbye") ||
-      localStorage.getItem("user_message").includes("see you") ||
-      localStorage.getItem("user_message").includes("see ya") ||
-      localStorage.getItem("user_message").includes("end") ||
-      isNewChat === true
-    ) {
-      console.log("store conversations");
-      await axios
-        .post("http://localhost:5000/conversations", {
-          user,
-          conversationTitle,
-          conversations,
-        })
-        .then((response) => {
-          if (response.status === 200) {
-            localStorage.removeItem("user_message");
-            setConversations("");
-          }
-        });
-    } else {
-    }
-  }
-};
-
 export const fetch_conversation_titles = async (setConversationTitles) => {
   await axios
     .get(`http://localhost:5000/fetch_conversation_titles/`, {
@@ -69,4 +33,26 @@ export const fetch_conversation_titles = async (setConversationTitles) => {
         setConversationTitles(response.data.titles);
       }
     });
+};
+
+export const error_message = (
+  setLoadingChatbotResponse,
+  setConversations,
+  botMessageIndex,
+  chatbotResponseId,
+  chatbotTypingResponse
+) => {
+  setLoadingChatbotResponse(false);
+  setConversations((prev) => {
+    const currentMessage = [...prev];
+    currentMessage[botMessageIndex].response = "Failed processing response";
+    return currentMessage;
+  });
+  setTimeout(() => {
+    const element = document.getElementById(chatbotResponseId);
+    if (element) {
+      chatbotTypingResponse(element, "Failed processing response");
+    } else {
+    }
+  }, 0);
 };
