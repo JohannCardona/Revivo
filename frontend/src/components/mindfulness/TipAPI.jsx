012bd9952a76f759851bf0b2498d0ff9fc6a5c6e
Johann Cardona <2007048@brunel.ac.uk>
2025-04-08 10:49:09 +0100

    Revert "Env URI"
    This reverts commit 23021117495aed57adf56602afeca2d590f04926.

--- frontend/src/components/mindfulness/TipAPI.jsx
+++ frontend/src/components/mindfulness/TipAPI.jsx
@@ -1,17 +1,18 @@
 import axios from "axios";
 
 export const store_tip = (user, currentTip, tipCategory) => {
-  return axios.post(
-    `${process.env.REACT_APP_BASE_URI}/store_tip/${tipCategory}`,
-    {
-      user,
-      currentTip,
-    },
-    {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${localStorage.getItem("token")}`,
+  return axios
+    .post(
+      `http://localhost:5000/store_tip/${tipCategory}`,
+      {
+        user,
+        currentTip,
       },
-    }
-  );
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      }
+    )
 };
