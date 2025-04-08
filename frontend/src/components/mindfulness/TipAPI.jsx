23021117495aed57adf56602afeca2d590f04926
Johann Cardona <2007048@brunel.ac.uk>
2025-04-08 10:34:12 +0100

    Env URI

--- frontend/src/components/mindfulness/TipAPI.jsx
+++ frontend/src/components/mindfulness/TipAPI.jsx
@@ -1,18 +1,17 @@
 import axios from "axios";
 
 export const store_tip = (user, currentTip, tipCategory) => {
-  return axios
-    .post(
-      `http://localhost:5000/store_tip/${tipCategory}`,
-      {
-        user,
-        currentTip,
+  return axios.post(
+    `${process.env.REACT_APP_BASE_URI}/store_tip/${tipCategory}`,
+    {
+      user,
+      currentTip,
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
       },
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${localStorage.getItem("token")}`,
-        },
-      }
-    )
+    }
+  );
 };
