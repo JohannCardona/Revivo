c77bfcf21031a43193297cd5de0915a8a1aab5b1
Johann Cardona <2007048@brunel.ac.uk>
2025-04-08 13:35:46 +0100

    Update ScrollToSection.jsx

--- frontend/src/components/ScrollToSection.jsx
+++ frontend/src/components/ScrollToSection.jsx
@@ -2,16 +2,18 @@ import { useMemo, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 const ScrollToSection = () => {
+  // Get current URL link object
   let location = useLocation();
-  console.log(location);
 
   let hashElement = useMemo(() => {
+    // Extract link element when change in location
     let hash = location.hash;
     const removeHashCharacter = (str) => {
       const result = str.slice(1);
       return result;
     };
 
+    // Remove hash symbol from URL and return link element
     if (hash) {
       let element = document.getElementById(removeHashCharacter(hash));
       return element;
@@ -21,6 +23,7 @@ const ScrollToSection = () => {
   }, [location]);
 
   useEffect(() => {
+    // Scroll to target section from extracted element
     if (hashElement) {
       hashElement.scrollIntoView({
         behavior: "smooth",
