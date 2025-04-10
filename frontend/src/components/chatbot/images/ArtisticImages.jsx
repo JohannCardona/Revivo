648ad7465083cc589fa3854cb9263f42880f2a9c
Johann Cardona <2007048@brunel.ac.uk>
2025-04-10 10:10:18 +0100

    Add comments to download image function

--- frontend/src/components/chatbot/images/ArtisticImages.jsx
+++ frontend/src/components/chatbot/images/ArtisticImages.jsx
@@ -15,9 +15,12 @@ export const generate_image = (prompt) => {
 
 export const downloadDALLEImage = (imgURL) => {
   if (!imgURL) return;
+  // Create a link element on the screen and target generated image URL
   const imageLink = document.createElement("a");
   imageLink.href = imgURL;
+  // Default image filename
   imageLink.download = "dalle_image.jpeg";
+  // Display file explorer to save image
   document.body.appendChild(imageLink);
   imageLink.click();
   document.body.removeChild(imageLink);
@@ -28,9 +31,12 @@ export const image_from_emotion = async (
   botMessageIndex,
   setDynamicChoice
 ) => {
+  // Get emotion value from local storage
   const storedEmotion = localStorage.getItem("mood");
+  // Pass prompt to DALL-E to generate image based on extracted emotion
   const dallePrompt = `An abstract artistic interpretation of the ${storedEmotion} mood in a creative, modern style.`;
   const imageUrl = await generate_image(dallePrompt);
+  // Save the image URL to the conversations array
   setConversations((prev) => {
     const currentMessage = [...prev];
     currentMessage[botMessageIndex].response = imageUrl;
