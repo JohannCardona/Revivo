c31d10ce622e47fe0bf41c9dafc20b168728a8e7
Johann Cardona <2007048@brunel.ac.uk>
2025-04-11 05:33:08 +0100

    Update classifiersSpeedComparison.py

--- backend/textEmotion/classifiersSpeedComparison.py
+++ backend/textEmotion/classifiersSpeedComparison.py
@@ -1,25 +1,22 @@
 import keras
 import transformers
-from transformers.optimization_tf import WarmUp
 import time
 
+# Code for the experiments on GPU server
 start = time.time()
 emotionClassifier = keras.models.load_model(
-    r"\checkpoint\distil_connect_drop2.h5", custom_objects={"TFDistilBertModel": transformers.TFDistilBertModel, "WarmUp": WarmUp})
-print(emotionClassifier)
+    r"Revivo\backend\textEmotion\checkpoint\distil_connect_drop2.h5", custom_objects={"TFDistilBertModel": transformers.TFDistilBertModel})
 end = time.time()
-print(f"Execution lasted: {end - start} sec")
+print(f"DistilBERT - Execution lasted: {end - start} sec")
 
 start1 = time.time()
 emotionClassifier1 = keras.models.load_model(
-    r"\textEmotion\checkpoint\model.h5", custom_objects={"TFBertModel": transformers.TFBertModel})
-print(emotionClassifier1)
+    r"C:\Users\JohCa\Documents\GitHub\Revivo\backend\textEmotion\checkpoint\model.h5", custom_objects={"TFBertModel": transformers.TFBertModel})
 end1 = time.time()
-print(f"Execution lasted: {end1 - start1} sec")
+print(f"BERT - Execution lasted: {end1 - start1} sec")
 
 start2 = time.time()
-emotionClassifier2 = keras.models.load_model(r"\checkpoint\tiny_bert_connect_drop2.h5", custom_objects={
-    "TFBertModel": transformers.TFBertModel, "WarmUp": WarmUp})
-print(emotionClassifier2)
+emotionClassifier2 = keras.models.load_model(r"C:\Users\JohCa\Documents\GitHub\Revivo\backend\textEmotion\checkpoint\tiny_bert_connect_drop2.h5", custom_objects={
+    "TFBertModel": transformers.TFBertModel})
 end2 = time.time()
-print(f"Execution lasted: {end2 - start2} sec")
+print(f"TinyBert - Execution lasted: {end2 - start2} sec")
