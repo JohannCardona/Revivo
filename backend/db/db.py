9975dbdc696c09dfed54b60b1fe8cd449b019158
Johann Cardona <2007048@brunel.ac.uk>
2025-04-11 05:50:13 +0100

    Update db.py

--- backend/db/db.py
+++ backend/db/db.py
@@ -2,8 +2,12 @@ from flask_pymongo import MongoClient
 from configparser import ConfigParser
 import os
 
+# Get config object for DB
 config = ConfigParser()
+# This is where the DB credentials come in
 config.read(os.path.abspath(os.path.join(".ini")))  
+# Store them in the DB client
 database = MongoClient(config["PROD"]["DB_URI"])
+# Instantiate DB object to use in other files
 mongo_db = database.revivo
 
